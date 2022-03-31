ba98a2033d809411d058d43ae81fe3691fa909dd
semwett0301 <90400355+semwett0301@users.noreply.github.com>
2022-04-01 00:27:50 +0300

    Exercise 4

--- main.py
+++ main.py
@@ -1,33 +1,26 @@
 import numpy as np
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import recall_score, precision_score, roc_auc_score, roc_curve, auc
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
-data = pd.read_csv("six.csv").set_index('competitorname').drop(index=['Milky Way', 'One dime', 'Fun Dip']).reset_index()
+data = pd.read_csv("fourth.csv").set_index("id")
+predict_data = data.drop("Class", axis=1)
+result_data = data["Class"]
 
-orig_data = pd.read_csv("six.csv")
+classificator_1 = KNeighborsClassifier(n_neighbors=1, p=2).fit(predict_data, result_data)
+print(classificator_1.kneighbors([[39, 22]], n_neighbors=1, return_distance=1))
 
-predict_data = data.drop(['competitorname', 'Y', 'winpercent'], axis=1)
-result_data = data['Y']
+classificator_2 = KNeighborsClassifier(n_neighbors=3, p=2).fit(predict_data, result_data)
+print(classificator_2.kneighbors([[39, 22]], n_neighbors=3))
 
-log = LogisticRegression().fit(predict_data, result_data)
+classificator_3 = KNeighborsClassifier(n_neighbors=3, p=2).fit(predict_data, result_data)
+print(classificator_3.predict([[39, 22]]))
 
-test_1_data = pd.read_csv("candy-test.csv").drop(
-    np.where(pd.read_csv("candy-test.csv")['competitorname'] != 'Tootsie Roll Midgies')[0]).drop(
-    ['competitorname', 'Y'], axis=1)
-test_2_data = pd.read_csv("candy-test.csv").drop(
-    np.where(pd.read_csv("candy-test.csv")['competitorname'] != 'Swedish Fish')[0]).drop(
-    ['competitorname', 'Y'], axis=1)
+classificator_4 = KNeighborsClassifier(n_neighbors=1, p=1).fit(predict_data, result_data)
+print(classificator_4.kneighbors([[39, 22]], n_neighbors=1, return_distance=1))
 
-print(log.predict_proba(test_1_data))
-print(log.predict_proba(test_2_data))
+classificator_5 = KNeighborsClassifier(n_neighbors=1, p=1).fit(predict_data, result_data)
+print(classificator_5.kneighbors([[39, 22]], n_neighbors=3))
 
-test_data = pd.read_csv("candy-test.csv")
-predict_data_test = test_data.drop(['competitorname', 'Y'], axis=1)
-
-print(recall_score(test_data['Y'], log.predict(predict_data_test)))
-print(precision_score(test_data['Y'], log.predict(predict_data_test)))
-
-fpr, tpr, thresholds = roc_curve(test_data['Y'], log.predict_proba(predict_data_test)[:,1], pos_label=1)
-print(auc(fpr, tpr))
+classificator_6 = KNeighborsClassifier(n_neighbors=3, p=1).fit(predict_data, result_data)
+print(classificator_6.predict([[39, 22]]))
