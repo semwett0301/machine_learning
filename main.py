9608b7f741e4190a3abea8ff5dac8542ec9e306e
semwett0301 <90400355+semwett0301@users.noreply.github.com>
2022-03-31 17:01:06 +0300

    Exercise 6

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import recall_score, precision_score, roc_auc_score, roc_curve, auc
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
-data = pd.read_csv("candy-data.csv").set_index('competitorname').drop(index=['Milky Way', 'Mr Good Bar']).reset_index()
+data = pd.read_csv("six.csv").set_index('competitorname').drop(index=['Milky Way', 'One dime', 'Fun Dip']).reset_index()
+
+orig_data = pd.read_csv("six.csv")
 
 predict_data = data.drop(['competitorname', 'Y', 'winpercent'], axis=1)
-result_data = data['winpercent']
+result_data = data['Y']
 
-lin = LinearRegression().fit(predict_data, result_data)
+log = LogisticRegression().fit(predict_data, result_data)
 
-test_1_data = pd.read_csv("candy-data.csv").drop(
-    np.where(pd.read_csv("candy-data.csv")['competitorname'] != 'Milky Way')[0]).drop(
-    ['competitorname', 'Y', 'winpercent'], axis=1)
-test_2_data = pd.read_csv("candy-data.csv").drop(
-    np.where(pd.read_csv("candy-data.csv")['competitorname'] != 'Mr Good Bar')[0]).drop(
-    ['competitorname', 'Y', 'winpercent'], axis=1)
+test_1_data = pd.read_csv("candy-test.csv").drop(
+    np.where(pd.read_csv("candy-test.csv")['competitorname'] != 'Tootsie Roll Midgies')[0]).drop(
+    ['competitorname', 'Y'], axis=1)
+test_2_data = pd.read_csv("candy-test.csv").drop(
+    np.where(pd.read_csv("candy-test.csv")['competitorname'] != 'Swedish Fish')[0]).drop(
+    ['competitorname', 'Y'], axis=1)
 
-print(lin.predict(test_1_data))
-print(lin.predict(test_2_data))
-print(lin.predict([[0, 1, 0, 1, 0, 1, 1, 0, 0, 0.93, 0.635]]))
+print(log.predict_proba(test_1_data))
+print(log.predict_proba(test_2_data))
+
+test_data = pd.read_csv("candy-test.csv")
+predict_data_test = test_data.drop(['competitorname', 'Y'], axis=1)
+
+print(recall_score(test_data['Y'], log.predict(predict_data_test)))
+print(precision_score(test_data['Y'], log.predict(predict_data_test)))
+
+fpr, tpr, thresholds = roc_curve(test_data['Y'], log.predict(predict_data_test), pos_label=0)
+print(auc(fpr, tpr))
