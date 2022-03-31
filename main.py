009cab9471f9db3d9e0dbe0fa056444230102636
semwett0301 <90400355+semwett0301@users.noreply.github.com>
2022-03-31 03:16:08 +0300

    Exercise 3

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
 import pandas as pd
-import seaborn as sns
 
-data = pd.read_csv("candy-data.csv")
+data = pd.read_csv("candy-data.csv").set_index('competitorname').drop(index=['Milky Way', 'Mr Good Bar']).reset_index()
 
+predict_data = data.drop(['competitorname', 'Y', 'winpercent'], axis=1)
+result_data = data['winpercent']
 
-lin = LinearRegression().fit(data[['chocolate', 'fruity', 'caramel', 'peanutyalmondy']], data[['winpercent']])
+lin = LinearRegression().fit(predict_data, result_data)
+
+test_1_data = pd.read_csv("candy-data.csv").drop(
+    np.where(pd.read_csv("candy-data.csv")['competitorname'] != 'Milky Way')[0]).drop(
+    ['competitorname', 'Y', 'winpercent'], axis=1)
+test_2_data = pd.read_csv("candy-data.csv").drop(
+    np.where(pd.read_csv("candy-data.csv")['competitorname'] != 'Mr Good Bar')[0]).drop(
+    ['competitorname', 'Y', 'winpercent'], axis=1)
+
+print(lin.predict(test_1_data))
+print(lin.predict(test_2_data))
+print(lin.predict([[0, 1, 0, 1, 0, 1, 1, 0, 0, 0.93, 0.635]]))
